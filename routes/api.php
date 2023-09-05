548526beeb55a9cec163bfa304e230091b8de778
habib24457 <habiburehman390@gmail.com>
2023-09-05 15:20:24 +0200

    Updated controller

--- routes/api.php
+++ routes/api.php
@@ -6,31 +6,28 @@ use App\Http\Controllers\PropertiesController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-//public routes
-Route::post('/login',[AuthController::class,'login']);
-Route::post('/register',[AuthController::class,'register']);
+// Public routes
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
 Route::get('/brokers', [BrokersController::class, 'index'])->name('brokers.index');
 Route::get('/brokers/{broker}', [BrokersController::class, 'show'])->name('brokers.show');
-Route::post('/brokers',[BrokersController::class,'store']);
-Route::get('/properties', [BrokersController::class, 'index'])->name('brokers.index');
-Route::get('/properties/{property}', [BrokersController::class, 'show'])->name('brokers.show');
+Route::get('/properties', [PropertiesController::class, 'index'])->name('brokers.index');
+Route::get('/properties/{property}', [PropertiesController::class, 'show'])->name('brokers.show');
 
-//Protected routes
-Route::group(['middleware'=>['auth:sanctum']], function(){
-    Route::post('/logout',[AuthController::class,'logout']);
+Route::apiResource('/properties', PropertiesController::class)->only(['store', 'update', 'destroy']);
+Route::apiResource('/brokers', BrokersController::class)->only(['store', 'update', 'destroy']);
+
+
+
+
+/*Protected routes: only the logged in users can access 
+*
+*/
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
 });
